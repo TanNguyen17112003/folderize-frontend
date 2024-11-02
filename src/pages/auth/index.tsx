9f2ad177d19f8b5dd789991a082f604b71d7a694
TruogAnh-28 <nguyentruongtuananh03@gmail.com>
2024-11-02 21:56:02 +0700

    feat: add login

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import { Stack } from '@mui/material';
+import LoginForm from 'src/sections/auth';
 
-function Index() {
-  return <div>Index</div>;
-}
+const Page = () => {
+  return (
+    <Stack
+      sx={{
+        maxHeight: '100vh',
+        overflow: 'auto',
+        bgcolor: 'white'
+      }}
+      className='min-h-screen'
+    >
+      <LoginForm />
+    </Stack>
+  );
+};
 
-export default Index;
+export default Page;
