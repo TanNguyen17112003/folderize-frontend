6836a0a64c97a11a2a73ac9a5b0310872cb8ec5d
TruogAnh-28 <nguyentruongtuananh03@gmail.com>
2024-11-03 11:33:11 +0700

    chore: seperate configtable, delete data.json

--- src/sections/documents/documents-list/index.tsx
+++ src/sections/documents/documents-list/index.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Box, Stack, TextField, Button, InputAdornment, Typography } from '@mui/material';
 import { SearchIcon } from 'lucide-react';
 import { CustomTable } from 'src/components/custom-table';
 import CustomTabs from 'src/components/CustomTabs';
 import usePagination from 'src/hooks/use-pagination';
-import DownloadIcon from '@mui/icons-material/Download';
-import BookmarkIcon from '@mui/icons-material/Bookmark';
-import CheckBoxIcon from '@mui/icons-material/CheckBox';
-import data from './data.json';
 import { useRouter } from 'next/router';
-
+import { docData } from 'src/types/document';
+import getDocumentManagementConfig from 'src/sections/documents/documents-list/documents-table-config';
+import useAppSnackbar from 'src/hooks/use-app-snackbar';
+import { Document } from 'src/types/document';
 function DocListIndex() {
   const [searchInput, setSearchInput] = React.useState('');
   const [selectedTab, setSelectedTab] = React.useState('all');
+  const { showSnackbarSuccess } = useAppSnackbar();
   const router = useRouter();
   const tabOptions = [
     { value: 'all', label: 'Tất cả' },
@@ -20,49 +20,21 @@ function DocListIndex() {
     { value: 'favorites', label: 'Đánh dấu' }
   ];
   const pagination = usePagination({
-    count: data.length,
+    count: docData.length,
     initialRowsPerPage: 10
   });
-  const configAll = [
-    { key: 'title', headerLabel: 'Tiêu đề' },
-    { key: 'description', headerLabel: 'Mô tả' },
-    { key: 'category', headerLabel: 'Loại' },
-    { key: 'lastModified', headerLabel: 'Lần chỉnh sửa' },
-    { key: 'views', headerLabel: 'Lượt xem' },
-    { key: 'fileSize', headerLabel: 'Kích thước' },
-    { key: 'format', headerLabel: 'Loại File' },
-    { key: 'uploader', headerLabel: 'Người đăng' },
-    { key: 'access', headerLabel: 'Quyền' },
-    {
-      key: 'bookmarked',
-      headerLabel: 'Đánh dấu',
-      renderCell: (row: any) =>
-        row.bookmarked ? <CheckBoxIcon color='primary' className='h-5 w-5 fill-secondary' /> : null
-    }
-  ];
-  const configBookmark = [
-    { key: 'title', headerLabel: 'Tiêu đề' },
-    { key: 'lastModified', headerLabel: 'Lần chỉnh sửa' },
-    { key: 'format', headerLabel: 'Loại File' },
-    { key: 'uploader', headerLabel: 'Người đăng' },
-    { key: 'access', headerLabel: 'Quyền' }
-  ];
+  const DocumentManagementConfig = useMemo(() => {
+    return getDocumentManagementConfig({
+      onClickDownload: (data: Document) => showSnackbarSuccess(`Đang tải tài liệu: ${data.title}`),
+      onClickBookmark: (data: Document) =>
+        showSnackbarSuccess(`Đã đánh dấu tài liệu: ${data.title}`)
+    });
+  }, [getDocumentManagementConfig]);
 
   const handleDetail = (row: any) => {
     console.log('Chi tiết hàng:', row);
     router.push('/documents/Details_doc');
   };
-
-  const actionList = (row: any) => (
-    <>
-      <Button className='hover:bg-secondary/15'>
-        <DownloadIcon className='h-5 w-5 fill-secondary' />
-      </Button>
-      <Button className='hover:bg-secondary/15'>
-        <BookmarkIcon className='h-5 w-5 fill-secondary' />
-      </Button>
-    </>
-  );
 
   return (
     <Box className='px-6 py-5'>
@@ -93,20 +65,18 @@ function DocListIndex() {
       <Box mt={2}>
         {selectedTab === 'all' && (
           <CustomTable
-            rows={data}
-            configs={configAll}
+            rows={docData}
+            configs={DocumentManagementConfig}
             onClickDetail={handleDetail}
-            renderRowActions={actionList}
             pagination={pagination}
           />
         )}
         {/* {selectedTab === 'organization' && <Typography>Tổ chức</Typography>} */}
         {selectedTab === 'favorites' && (
           <CustomTable
-            rows={data}
-            configs={configBookmark}
+            rows={docData}
+            configs={DocumentManagementConfig}
             onClickDetail={handleDetail}
-            renderRowActions={actionList}
             pagination={pagination}
           />
         )}
