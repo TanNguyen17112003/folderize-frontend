e63c06950a11eaeda8d8ceacbc0d24718053592f
Minhtuei <tue.tran20032003@gmail.com>
2024-11-05 22:36:44 +0700

    feat: implement new document list item

--- src/sections/documents/documents-list/index.tsx
+++ src/sections/documents/documents-list/index.tsx
@@ -1,5 +1,5 @@
 import React, { useMemo } from 'react';
-import { Box, Stack, TextField, Button, InputAdornment, Typography } from '@mui/material';
+import { Box, Stack, TextField, Button, InputAdornment, Typography, Card } from '@mui/material';
 import { SearchIcon } from 'lucide-react';
 import { CustomTable } from 'src/components/custom-table';
 import CustomTabs from 'src/components/CustomTabs';
@@ -9,15 +9,16 @@ import { docData } from 'src/types/document';
 import getDocumentManagementConfig from 'src/sections/documents/documents-list/documents-table-config';
 import useAppSnackbar from 'src/hooks/use-app-snackbar';
 import { Document } from 'src/types/document';
+import Grid from '@mui/material/Grid2';
+import Image from 'next/image';
 function DocListIndex() {
   const [searchInput, setSearchInput] = React.useState('');
   const [selectedTab, setSelectedTab] = React.useState('all');
   const { showSnackbarSuccess } = useAppSnackbar();
   const router = useRouter();
   const tabOptions = [
-    { value: 'all', label: 'Tất cả' },
-    // { value: 'organization', label: 'Tổ chức' },
-    { value: 'favorites', label: 'Đánh dấu' }
+    { value: 'all', label: 'Tất cả', content: 'All Documents' },
+    { value: 'favorites', label: 'Đánh dấu', content: 'Favorite Documents' }
   ];
   const pagination = usePagination({
     count: docData.length,
@@ -64,12 +65,46 @@ function DocListIndex() {
 
       <Box mt={2}>
         {selectedTab === 'all' && (
-          <CustomTable
-            rows={docData}
-            configs={DocumentManagementConfig}
-            onClickDetail={handleDetail}
-            pagination={pagination}
-          />
+          // <CustomTable
+          //   rows={docData}
+          //   configs={DocumentManagementConfig}
+          //   onClickDetail={handleDetail}
+          //   pagination={pagination}
+          // />
+          <Grid container spacing={4}>
+            {docData.map((doc) => (
+              <Grid key={doc.id}>
+                <Card
+                  sx={{
+                    width: 200,
+                    height: 338,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    cursor: 'pointer',
+                    '&:hover': {
+                      border: '1px solid #2970FF'
+                    }
+                  }}
+                >
+                  <Box
+                    height={200}
+                    width={200}
+                    overflow='hidden'
+                    display='flex'
+                    justifyContent='center'
+                    alignItems='center'
+                  >
+                    <Image src={'/ui/pdfIcon.png'} alt='pdf' width={100} height={100} />
+                  </Box>
+                  <Box p={2} alignItems={'center'} display={'flex'}>
+                    <Typography variant='subtitle2' className='truncate'>
+                      {doc.title}
+                    </Typography>
+                  </Box>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
         )}
         {/* {selectedTab === 'organization' && <Typography>Tổ chức</Typography>} */}
         {selectedTab === 'favorites' && (
